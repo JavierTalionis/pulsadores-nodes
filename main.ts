28e1a03dde5fab8e6d571a075f7379fb29b8132a
Javier Pereira <92329076+JavierTalionis@users.noreply.github.com>
2021-10-12 12:05:11 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,13 +5,26 @@ radio.onReceivedNumber(function (receivedNumber) {
         pins.digitalWritePin(DigitalPin.P0, 1)
         Final = false
         while (!(Final)) {
-        	
+            let timer = 0
+            // Push the button. Final=OK
+            if (input.buttonIsPressed(Button.A)) {
+                // KO=0
+                reply = 1
+                Final = true
+            }
+            // If timeout Final=KO
+            if (timer > 10000) {
+                // KO=0
+                reply = 0
+                Final = true
+            }
         }
     }
 })
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.B, function () {
     basic.showString("" + (Math.abs(control.eventTimestamp())))
 })
+let reply = 0
 let Final = false
 radio.setGroup(1)
 let MyNode = 1
