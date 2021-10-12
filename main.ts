9ecc8d7fe1a49568a12cecac41aaff2d58cffac8
Javier Pereira <92329076+JavierTalionis@users.noreply.github.com>
2021-10-12 11:42:36 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    // It's me
+    if (receivedNumber == 1) {
+        // Turn on the led
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    }
+})
+radio.setGroup(1)
+let MyNode = 1
 basic.forever(function () {
 	
 })
