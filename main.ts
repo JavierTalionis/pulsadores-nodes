a388a41a64b9eed48544849dac6075d6d208a23f
Javier Pereira <92329076+JavierTalionis@users.noreply.github.com>
2021-10-12 11:58:04 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,11 +3,16 @@ radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         // Turn on the led
         pins.digitalWritePin(DigitalPin.P0, 1)
+        Final = false
+        while (!(Final)) {
+        	
+        }
     }
 })
 input.onButtonPressed(Button.A, function () {
     basic.showString("" + (Math.abs(control.eventTimestamp())))
 })
+let Final = false
 radio.setGroup(1)
 let MyNode = 1
 basic.forever(function () {
